060f350bc96e2e089b9758ec0b3663772bf1f02b
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-03-29 22:44:07 -0400

    checking for default object

--- test/unit/can_configuration_test.rb
+++ test/unit/can_configuration_test.rb
@@ -1,11 +1,17 @@
 require_relative '../../lib/user_cans.rb'
 
 describe "Configuration" do
-  it "should return a default config" do
+  it "should return a config object if block not given" do
     expect(Can::UserCans.configure).not_to eq(nil)
   end
 
   it "should accept a block" do
     expect { |b| Can::UserCans.configure(&b) }.to yield_control
   end
+
+  it "should return a default object" do
+    config = Can::UserCans.configure
+    expect(config.cans_klass).to eq(Can::UserCans)
+  end
+
 end
