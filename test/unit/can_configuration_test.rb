4f644ab3678f1b2423b6314ea5b41b53b0b85a94
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-03-29 22:34:49 -0400

    testing for configuration

--- test/unit/can_configuration_test.rb
+++ test/unit/can_configuration_test.rb
@@ -4,4 +4,8 @@ describe "Configuration" do
   it "should return a default config" do
     expect(Can::UserCans.configuration).not_to eq(nil)
   end
+
+  it "should accept a block" do
+    expect({ |b| Can::UserCans.configure(&b) }.to yield_control
+  end
 end
