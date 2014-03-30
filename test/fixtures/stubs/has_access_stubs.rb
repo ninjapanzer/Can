3fd9a3437fa9772a440cf92572d012a6c3abe6df
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-03-30 01:07:17 -0400

    Creating stubs to unit test has access

---
+++ test/fixtures/stubs/has_access_stubs.rb
@@ -0,0 +1,43 @@
+module Can
+  module UserCans
+    
+    def self.configure
+      ConfigurationStub.new
+    end
+
+    def self.find_for fake
+      fake
+    end
+    
+    class ConfigurationStub
+      
+      def cans_klass
+        UserCans
+      end
+
+      class Default; end
+    end
+  end
+end
+
+class AccessibleControllerStub
+  include Can::HasAccess
+  def current_user
+    self
+  end
+
+  def can? fake
+    true
+  end
+end
+
+class InaccessibleControllerStub
+  include Can::HasAccess
+  def current_user
+    self
+  end
+
+  def can? fake
+    true
+  end
+end
--- test/unit/can_has_access_test.rb
+++ test/unit/can_has_access_test.rb
@@ -1,11 +1,16 @@
 require_relative "../../lib/has_access.rb"
+require_relative "../fixtures/stubs/has_access_stubs.rb"
 
-class HasAccessStub
-  include Can::HasAccess
-end
+class CanHasAccessTest
 
-describe "HasAccess" do
-  it "should respond to has_access?" do
-    expect(HasAccessStub.new).to respond_to :has_access?
+  describe "HasAccess" do
+    it "should respond to has_access?" do
+      expect(AccessibleControllerStub.new).to respond_to :has_access?
+    end
+
+    it "should not redirect on has_access?" do
+      expect(AccessibleControllerStub.new.has_access?).to eq(nil)
+    end
   end
+
 end
