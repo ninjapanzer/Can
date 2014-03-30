32771da2edcd7eb6496d651df4da1b7f7a3bbb16
Paul Scarrone <pscarrone@thinkthroughmath.com>
2014-03-29 22:36:39 -0400

    params are hard

--- test/unit/can_configuration_test.rb
+++ test/unit/can_configuration_test.rb
@@ -2,10 +2,10 @@ require_relative '../../lib/user_cans.rb'
 
 describe "Configuration" do
   it "should return a default config" do
-    expect(Can::UserCans.configuration).not_to eq(nil)
+    expect(Can::UserCans.configure).not_to eq(nil)
   end
 
   it "should accept a block" do
-    expect({ |b| Can::UserCans.configure(&b) }.to yield_control
+    expect { |b| Can::UserCans.configure(&b) }.to yield_control
   end
 end
